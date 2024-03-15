f227a208e5d90dcc8ab0dc1baa4e269706114465
ericcccliu <liueric360@gmail.com>
2024-03-15 13:43:49 -0500

    add conversation router to index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from api.routes.auth_routes import router as auth_router
+from api.routes.conversation_routes import router as conversation_router
 # Import other necessary modules and dependencies
 
 app = FastAPI()
 
 # Include the authentication routes
 app.include_router(auth_router)
+app.include_router(conversation_router)
