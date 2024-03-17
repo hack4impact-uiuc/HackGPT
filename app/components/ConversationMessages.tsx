de9aa9ce1210a1aea5ad8191b7b49b1a4c60bac4
ericcccliu <liueric360@gmail.com>
2024-03-17 01:34:56 -0500

    add scrolling

--- app/components/ConversationMessages.tsx
+++ app/components/ConversationMessages.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Box, Text, VStack } from "@chakra-ui/react";
 
 export interface Message {
@@ -15,6 +15,12 @@ const ConversationMessages: React.FC<ConversationMessagesProps> = ({
   messages,
   userName = "User",
 }) => {
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+  
   return (
     <VStack spacing={4} align="stretch" width="100%" mb={10}>
       {messages.map((message, index) => (
@@ -33,6 +39,7 @@ const ConversationMessages: React.FC<ConversationMessagesProps> = ({
           <Text whiteSpace="pre-wrap">{message.content}</Text>
         </Box>
       ))}
+      <div ref={messagesEndRef} />
     </VStack>
   );
 };
